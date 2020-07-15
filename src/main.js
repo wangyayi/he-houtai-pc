39179f8c7f156d3cd35d7b8482819e48f08d71ce
wangyayi <413946849@qq.com>
2020-07-15 20:01:38 +0800

    完成登录组件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+//导入vue
+import Vue from 'vue';
+//导入依赖的ui模块
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+//导入根组件
+import App from './App.vue';
+//导入自己的路由模块
+import router from '@/router';
 
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/views/login/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container-login">
+    <el-card class="box-card">
+      <div>
+        <img src="../../assets/logo_index.png" alt />
+      </div>
+      <el-form ref="form" :model="form">
+        <el-form-item>
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号码"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            v-model="loginForm.code"
+            placeholder="请输入验证码"
+            style="width:235px;margin-right:10px"
+          ></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%">登 录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "page-login",
+  data() {
+    return {
+      loginForm: {
+        mobile: "",
+        code: ""
+      }
+    };
+  }
+};
+</script>
+
+<style scoped lang='less'>
+.container-login {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: url("../../assets/login_bg.jpg") no-repeat center/cover;
+  .box-card {
+    width: 400px;
+    height: 350px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    img {
+      display: block;
+      width: 200px;
+      margin: 0 auto 20px;
+    }
+  }
+}
+</style>
