adb517d70f694e8b0a2f5ab4582932de7d598277
wangyayi <413946849@qq.com>
2020-07-24 11:29:35 +0800

    封面图片

---
+++ src/components/my-image.vue
@@ -0,0 +1,194 @@
+<template>
+  <div class="my-image">
+    <!-- 图片按钮 -->
+    <div class="btn-image" @click="openDialog()">
+      <!-- 如果父组件有传递数据过来就用父组件的数据，没有传值就用默认地址 -->
+      <img :src="value || previewImg" alt />
+    </div>
+    <!-- 对话框，默认隐藏 -->
+    <el-dialog :visible.sync="dislogVisible" width="720">
+      <el-tabs v-model="activeName" type="card">
+        <el-tab-pane label="素材库" name="images">
+          <!-- 按钮切换 -->
+          <el-radio-group @change="toggleList" v-model="reqParams.collect" size="small">
+            <el-radio-button label="false">全部</el-radio-button>
+            <el-radio-button label="true">收藏</el-radio-button>
+          </el-radio-group>
+          <!-- 图片列表 -->
+
+          <div class="image-list">
+            <div
+              :class="{selected:selectedImgUrl===item.url}"
+              @click="selectedImg(item.url)"
+              class="image-item"
+              v-for="item in images"
+              :key="item.id"
+            >
+              <img :src="item.url" alt />
+            </div>
+          </div>
+          <!-- 分页 -->
+          <el-pagination
+            background
+            layout="prev, pager, next"
+            :total="total"
+            :page-size="reqParams.per_page"
+            :current-page="reqParams.page"
+            @current-change="changePaper"
+          ></el-pagination>
+        </el-tab-pane>
+        <el-tab-pane label="上传图片" name="upload">
+          <!-- show-file-list是否显示已上传文件列表 -->
+          <el-upload
+            class="avatar-uploader"
+            action="http://ttapi.research.itcast.cn/mp/v1_0/user/images"
+            :show-file-list="false"
+            :on-success="uploadSuccess"
+            :headers="headers"
+            name="image"
+          >
+            <img v-if="uploadImgUrl" :src="uploadImgUrl" class="avatar" />
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+        </el-tab-pane>
+      </el-tabs>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dislogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="confirmImg">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import auth from "@/utils/auth";
+//导入本地图片，因为动态src的图片不会打包到服务器上，所以图片显示不出来，
+// 通过import导入的资源会打包
+import img from "../assets/default.png";
+export default {
+  name: "my-image",
+  // 接收父组件传递过来的值
+  props: ["value"],
+  data() {
+    return {
+      dislogVisible: false,
+      activeName: "images",
+      reqParams: {
+        collect: false,
+        page: 1,
+        per_page: 8,
+      },
+      total: 0,
+      images: [],
+      //当前选中的图片地址
+      selectedImgUrl: null,
+      uploadImgUrl: null,
+
+      headers: {
+        Authorization: `Bearer ${auth.getUser().token}`,
+      },
+      //封面预览图，通过import导入进来的图片
+      previewImg: img,
+    };
+  },
+  methods: {
+    //确认图片
+    confirmImg() {
+      if (this.activeName === "images") {
+        //激活素材库
+        if (!this.selectedImgUrl)
+          return this.$message.warning("请选择一张图片");
+        // this.previewImg = this.selectedImgUrl;
+        //触发自定义事件，把选中的图片提交给父组件使用
+        this.$emit("input", this.selectedImgUrl);
+      } else {
+        //激活上传图片
+        if (!this.uploadImgUrl) {
+          return this.$message.warning("请上传一张图片");
+        } else {
+          // this.previewImg = this.uploadImgUrl;
+          this.$emit("input", this.uploadImgUrl);
+        }
+      }
+      // 完成功能后关闭选项卡
+      this.dislogVisible = false;
+    },
+    uploadSuccess(res) {
+      this.uploadImgUrl = res.data.url;
+    },
+    selectedImg(url) {
+      this.selectedImgUrl = url;
+    },
+    changePaper(newPage) {
+      this.reqParams.page = newPage;
+      this.getImage();
+    },
+    toggleList() {
+      this.reqParams.page = 1;
+      this.getImage();
+    },
+    openDialog() {
+      this.dislogVisible = true;
+      this.getImage();
+      //打开对话框清除之前的记录
+      this.selectedImgUrl = null;
+      this.uploadImgUrl = null;
+      this.activeName = "images";
+    },
+    async getImage() {
+      const res = await this.$http.get("user/images", {
+        params: this.reqParams,
+      });
+      this.images = res.data.data.results;
+      this.total = res.data.data.total_count;
+    },
+  },
+};
+</script>
+
+<style scoped lang="less">
+.my-image {
+  display: inline-block;
+  margin-right: 20px;
+  .btn-image {
+    width: 150px;
+    height: 150px;
+
+    border: 1px dashed #ddd;
+    img {
+      width: 100%;
+      height: 100%;
+      display: block;
+    }
+  }
+  .image-list {
+    margin-top: 15px;
+    .image-item {
+      width: 150px;
+      height: 120px;
+      display: inline-block;
+      border: 1px solid #ddd;
+      margin-right: 15px;
+      position: relative;
+      img {
+        width: 100%;
+        height: 100%;
+        display: block;
+      }
+      &.selected::after {
+        content: "";
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        background: rgba(0, 0, 0, 0.3) url("../assets/selected.png") no-repeat
+          center / 50px auto;
+      }
+    }
+  }
+}
+::v-deep .el-dialog__body {
+  padding-bottom: 0px;
+}
+</style>
--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -9,8 +9,28 @@
         <el-form-item label="标题:">
           <el-input v-model="articalTitle.name"></el-input>
         </el-form-item>
-        <el-form-item label="内容:"></el-form-item>
-        <el-form-item label="封面:"></el-form-item>
+        <el-form-item label="内容:" class="el-item">
+          <quill-editor v-model="articalTitle.content" :options="editorOption" />
+        </el-form-item>
+        <el-form-item label="封面:">
+          <!-- 当切换封面图数据的时候，图片中的数据要进行重置,用change事件实现 -->
+          <el-radio-group @change="articalTitle.cover.images=[]" v-model="articalTitle.cover.type">
+            <el-radio :label="1">单图</el-radio>
+            <el-radio :label="3">三图</el-radio>
+            <el-radio :label="0">无图</el-radio>
+            <el-radio :label="-1">自动</el-radio>
+          </el-radio-group>
+          <!-- 只有一张图的情况 -->
+          <div style="margin-top:10px" v-if="articalTitle.cover.type===1">
+            <!-- 父子组件传值，子组件上传一张图片图片地址传给父组件 -->
+            <!-- <my-image :value="testImg" @input="testImg=$event"></my-image> -->
+            <my-image v-model="articalTitle.cover.images[0]"></my-image>
+          </div>
+          <!-- 需要三张图的情况 -->
+          <div style="margin-top:10px" v-if="articalTitle.cover.type===3">
+            <my-image v-model="articalTitle.cover.images[i-1]" v-for="i in 3" :key="i"></my-image>
+          </div>
+        </el-form-item>
         <el-form-item label="频道:">
           <my-channel v-model="articalTitle.channle_id"></my-channel>
         </el-form-item>
@@ -24,18 +44,55 @@
 </template>
 
 <script>
+import "quill/dist/quill.core.css";
+import "quill/dist/quill.snow.css";
+import "quill/dist/quill.bubble.css";
+//导入富文本组件
+import { quillEditor } from "vue-quill-editor";
+
 export default {
   name: "page-publish",
+  //使用富文本组件
+  components: {
+    quillEditor,
+  },
+
   data() {
     return {
       articalTitle: {
         name: "",
-        channle_id: null
-      }
+        channle_id: null,
+        content: null,
+        cover: {
+          type: 1,
+          //图片数据
+          images: [],
+        },
+      },
+      editorOption: {
+        placeholder: "请输入文章内容",
+        modules: {
+          toolbar: [
+            ["bold", "italic", "underline", "strike"],
+            ["blockquote", "code-block"],
+            [{ header: 1 }, { header: 2 }],
+            [{ list: "ordered" }, { list: "bullet" }],
+            [{ indent: "-1" }, { indent: "+1" }],
+            ["image"],
+          ],
+        },
+      },
     };
-  }
+  },
 };
 </script>
 
 <style scoped lang="less">
+// ::v-deep深度作用符号，让选择器在其它组件下生效
+::v-deep .quill-editor {
+  height: 200px;
+}
+::v-deep .el-item {
+  margin-bottom: 70px;
+}
 </style>
